f50ef7f4201e8b39ead867fe64bf48bcfd311c6c
chunchengxiu <69743053+chunchengxiu@users.noreply.github.com>
2020-08-18 15:20:54 +0800

    Update Program.cs
    test

--- WebApi.Net/ConsoleAppTest/Program.cs
+++ WebApi.Net/ConsoleAppTest/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleAppTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ccxiu!");
+            
         }
     }
 }
